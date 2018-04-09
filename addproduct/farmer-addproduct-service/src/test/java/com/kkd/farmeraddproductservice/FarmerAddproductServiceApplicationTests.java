be2f738bdaf2e264b81f1cf74d818191676dbb27
sridharan <sridharan11152@gmail.com>
2018-04-09 08:51:46 +0530

    test cases added for farmer-addproduct-service

--- addproduct/farmer-addproduct-service/src/test/java/com/kkd/farmeraddproductservice/FarmerAddproductServiceApplicationTests.java
+++ addproduct/farmer-addproduct-service/src/test/java/com/kkd/farmeraddproductservice/FarmerAddproductServiceApplicationTests.java
@@ -1,16 +1,51 @@
 package com.kkd.farmeraddproductservice;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(value = FarmerAddproductServiceController.class, secure = false)
 public class FarmerAddproductServiceApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
+	@MockBean
+	private FarmerAddproductServiceApplicationProxy proxy;
+	private FarmerProduct farmerProduct;
+    
+    private FarmerProduct mockFarmerProduct = new FarmerProduct("productId","imageUrl","productName","description",10,8,100,true);
+	String FarmerProductJson = "{\"productId\":\"productId\",\"imageUrl\":\"imageUrl\",\"productName\":\"productName\",\"description\":\"description\",\"price\":10,\"bulkOrderPrice\":8,\"quantity\":100,\"availability\":true}";
 
 	@Test
-	public void contextLoads() {
+	public void addProductTest() throws Exception {
+		String id  = "farmer_id";
+		Mockito.when(
+				proxy.addProduct(Mockito.anyString(),
+						Mockito.any(FarmerProduct.class))).thenReturn(id);
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.post(
+				"/farmer/1/product").accept(
+				MediaType.APPLICATION_JSON).content(FarmerProductJson)
+				.contentType(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+		String expected = "farmer_id";
+		assertEquals(expected,response.getContentAsString());
 	}
 
 }
